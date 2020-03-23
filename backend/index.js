6c7b19f432bafd776742c66cbe412bb38b2be941
max-ribeiro <max_ribeiro@live.com>
2020-03-23 12:54:08 -0300

    Hello World!

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    return res.json({
+        message:"Hello World!",
+        event:"Semana Omnistack11",
+        name:"Max"
+    });
+});
+
 app.listen(3333);
