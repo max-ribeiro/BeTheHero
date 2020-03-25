4295021680ad50fe3e974952d10572e963127727
max-ribeiro <max_ribeiro@live.com>
2020-03-25 19:30:33 -0300

    integrated the frontend of create ong page to backend

--- README.md
+++
@@ -1,8 +0,0 @@
-# Be the hero
-## Resumo
-Projeto da semana omnistack 11 da Rocketseat.  
-
-Be The Hero visa ser uma plataforma onde ONGs podem se cadastrar e registrar suas campanhas. Através de Be the hero, voluntarios terão acesso as campanhas criadas pelas ONGs e poderão colaborar com as mesmas.
-
-## API
-A API criada neste projeto ira permitir que o frontend da aplicação possa utilizar dos dados cadastrados no backend de varias formas, seja no desenvolvimento da pagina web ou da aplicação mobile.  
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,37 +1,96 @@
-import React from 'react';
-import {FiArrowLeft} from 'react-icons/fi';
-import {Link} from 'react-router-dom';  
+import React, { useState } from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link, useHistory} from 'react-router-dom';
 
+import api from '../../services/api';
 import './style.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
-    return(
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsApp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+    
+    async function handleRegister(e) {
+        e.preventDefault();
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        };
+        
+        try{
+            const response = await api.post('ongs',data); 
+            alert(`Cadastro efetuado com sucesso, seu id de acesso é ${response.data.id}`)
+            history.push('/');
+        }catch(error){
+            alert('Erro ao efetuar o cadastro, tente novamente.');
+        }
+        
+    }
+
+    return (
         <div className="register-container">
             <div className="content">
                 <section>
-                    <img src={logoImg} alt="Be the hero"/>
+                    <img src={logoImg} alt="Be the hero" />
+
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     <Link to="/" className="back-link">
-                        <FiArrowLeft size={16} color="#E02041"/>
+                        <FiArrowLeft size={16} color="#E02041" />
                         Voltar
                     </Link>
                 </section>
-                <form>
-                    <input type="text" placeholder="Nome da ONG"/>
-                    <input type="text" placeholder="E-Mail"/>
-                    <input type="text"  placeholder="Whatsapp"/>
+
+                <form onSubmit={handleRegister}>
+                    <input
+                        required
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+
+                    <input
+                        required
+                        type="email"
+                        placeholder="E-Mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        placeholder="Whatsapp"
+                        value={whatsapp}
+                        onChange={e => setWhatsApp(e.target.value)}
+                    />
+
                     <div className="input-group">
-                        <input type="text"  placeholder="Cidade"/>
-                        <input type="text"  placeholder="UF" style={{
-                            "width":80
-                        }}/>
+                        <input
+                            required
+                            placeholder="Cidade"
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+
+                        <input
+                            required
+                            placeholder="UF"
+                            style={{"width": 80}}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
                     </div>
-                    <div className="button" type="submit">
-                        Registrar
-                    </div>
+                    <button className="button" type="submit">
+                        Cadastrar
+                    </button>
                 </form>
             </div>
         </div>
