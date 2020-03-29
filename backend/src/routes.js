06d1798de3f2ea3c36cbb07850ebceb37459056d
max-ribeiro <max_ribeiro@live.com>
2020-03-29 10:38:53 -0300

    finished the validation of routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,26 +7,55 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
-
-routes.post('/sessions',SessionController.create);
-
+// Session
+routes.post('/sessions',celebrate({
+    [Segments.BODY]:Joi.object().keys({
+        ong_id:Joi.string().length(8).required(),
+    }),
+}),SessionController.create);
+// Ong
 routes.get('/ongs',OngController.index);//Busca ongs registradas
+
 routes.post('/ongs',celebrate({
     [Segments.BODY]:Joi.object().keys({
         name:Joi.string().required(),
         email:Joi.string().required().email(),
-        whatsapp:Joi.number().required().min(10).max(11),
+        whatsapp:Joi.string().required().min(10).max(11),
         city:Joi.string().required(),
         uf:Joi.string().length(2)
     }),
 }),OngController.create);//Cria nova ong
 routes.delete('/ongs/:id',OngController.delete);//Deleta ong criada
+// Profile
+routes.get('/profile',celebrate({
+    [Segments.HEADERS]:Joi.object({
+        authorization:Joi.string().required(),
+    }).unknown(),
+}),ProfileController.index);
+// Incidents
+routes.get('/incidents',celebrate({
+    [Segments.QUERY]:Joi.object().keys({
+        page:Joi.number(),
+    }),
+}),IncidentController.index);
 
-routes.get('/incidents',IncidentController.index);
-routes.post('/incidents',IncidentController.create);
-routes.delete('/incidents/:id',IncidentController.delete);
+routes.post('/incidents',celebrate({
+    [Segments.BODY]:Joi.object().keys({
+        title:Joi.string().required(),
+        description:Joi.string().required(),
+        value:Joi.number().min(1).required(),
+    }),
+    [Segments.HEADERS]:Joi.object({
+        authorization:Joi.string().length(8).required(),
+    }).unknown(),
+}),IncidentController.create);
 
-routes.get('/profile',ProfileController.index);
+routes.delete('/incidents/:id',celebrate({
+    [Segments.PARAMS]:Joi.object().keys({
+        id:Joi.string().required(),
+    }),
+}),IncidentController.delete);
+
 
 
 module.exports = routes;
